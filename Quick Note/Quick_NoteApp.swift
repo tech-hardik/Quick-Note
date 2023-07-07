d4cd010a40534c98487fe53bd332423792347352
HARDIK SHARMA <hardik.tech@outlook.com>
2023-07-07 19:38:08 +0530

    Initial Commit

---
+++ Quick Note/Quick_NoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quick_NoteApp.swift
+//  Quick Note
+//
+//  Created by HARDIK SHARMA on 07/07/23.
+//
+
+import SwiftUI
+
+@main
+struct Quick_NoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
